1ec2611011321bbcf58a933dde00233397260483
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-08-20 01:11:44 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { rows } from './data';
+import Thead from './table/thead';
+import Tbody from './table/tbody';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Component: React.FC = () => (
+  <div>
+    <h1>健康に関する調査</h1>
+    <table>
+      <Thead />
+      <Tbody rows={rows} />
+    </table>
+  </div>
+);
 
-export default App;
+export default Component;
